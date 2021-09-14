c15750fe4666a03cf3f58a091f338f3bd3df1a2e
salmanahmad2 <hafizmaliksalman@gmail.com>
2021-09-14 12:18:02 +0500

    initial commit

---
+++ database/db.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func DatabaseConnection() {
+	db, err := sql.Open("mysql", "root:salman12@tcp(127.0.0.1:3306)/calculator")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("db is connected")
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
